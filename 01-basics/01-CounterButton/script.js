45c05976636932dc293f3ef71029933b486a1ddf
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-12-18 16:48:47 +0300

    add solution for 01-CounterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -2,3 +2,16 @@ import Vue from './vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+export const app = new Vue({
+  el: '#app',
+  data () {
+    return  {
+      counter: 0
+    }
+  },
+  methods: {
+    counterButton () {
+      this.counter++
+    }
+  },
+})
