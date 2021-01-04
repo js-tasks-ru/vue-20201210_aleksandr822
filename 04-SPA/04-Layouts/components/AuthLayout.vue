7e8dd29c68d3e9ee0bd9ff422ebe55a4358afa24
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2021-01-04 18:18:59 +0300

    add solution for 04-SPA/04-Layouts

--- 04-SPA/04-Layouts/components/AuthLayout.vue
+++ 04-SPA/04-Layouts/components/AuthLayout.vue
@@ -1,9 +1,42 @@
-<template></template>
+<template>
+  <main class="main">
+    <div class="page page_onboarding">
+      <div class="container">
+        <h1 class="page__title text-center">{{title}}</h1>
+        <form class="form">
+          <div class="form-group">
+            <label class="form-label">Email</label>
+            <div class="input-group">
+              <input type="email" placeholder="demo@email" class="form-control" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label">Пароль</label>
+            <div class="input-group">
+              <input type="password" placeholder="password" class="form-control" />
+            </div>
+          </div>
+          <div class="form__buttons">
+            <button type="submit" class="button button_primary button_block">Войти</button>
+          </div>
+          <div class="form__append">Нет аккаунта? <a href="register.html" class="link">Зарегистрируйтесь</a></div>
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
 
 <script>
 export default {
   name: 'AuthLayout',
+  props: {
+    title: {
+      type: String
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- 04-SPA/04-Layouts/components/FormLayout.vue
+++ 04-SPA/04-Layouts/components/FormLayout.vue
@@ -1,8 +1,52 @@
-<template></template>
+<template>
+  <div class="container">
+    <h2 class="page__title">{{title}}</h2>
+    <fieldset class="form-section">
+      <div class="form-group">
+        <label class="form-label">Название</label>
+        <div class="input-group">
+          <input class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Дата</label>
+        <div class="input-group">
+          <input type="date" class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Место проведения</label>
+        <div class="input-group">
+          <input class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Описание</label>
+        <div class="input-group">
+          <textarea rows="3" class="form-control"></textarea>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Изображение</label>
+        <div class="image-uploader">
+          <label class="image-uploader__preview">
+            <span>Загрузить изображение</span>
+            <input type="file" accept="image/*" class="form-control-file" />
+          </label>
+        </div>
+      </div>
+    </fieldset>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'FormLayout',
+  props: {
+    title: {
+      type: String
+    }
+  }
 };
 </script>
 
