0ca320a339f20578f090e073a8cb645b5350fcfa
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2021-01-07 13:24:25 +0300

    change Auth and Form components, add slot.

--- 04-SPA/04-Layouts/components/AuthLayout.vue
+++ 04-SPA/04-Layouts/components/AuthLayout.vue
@@ -3,24 +3,7 @@
     <div class="page page_onboarding">
       <div class="container">
         <h1 class="page__title text-center">{{title}}</h1>
-        <form class="form">
-          <div class="form-group">
-            <label class="form-label">Email</label>
-            <div class="input-group">
-              <input type="email" placeholder="demo@email" class="form-control" />
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="form-label">Пароль</label>
-            <div class="input-group">
-              <input type="password" placeholder="password" class="form-control" />
-            </div>
-          </div>
-          <div class="form__buttons">
-            <button type="submit" class="button button_primary button_block">Войти</button>
-          </div>
-          <div class="form__append">Нет аккаунта? <a href="register.html" class="link">Зарегистрируйтесь</a></div>
-        </form>
+        <slot />
       </div>
     </div>
   </main>
@@ -38,5 +21,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
--- 04-SPA/04-Layouts/views/FormPage.vue
+++ 04-SPA/04-Layouts/views/FormPage.vue
@@ -1,6 +1,41 @@
 <template>
   <form-layout title="Создать митап">
     <form>Meetup Form</form>
+    <fieldset class="form-section">
+      <div class="form-group">
+        <label class="form-label">Название</label>
+        <div class="input-group">
+          <input class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Дата</label>
+        <div class="input-group">
+          <input type="date" class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Место проведения</label>
+        <div class="input-group">
+          <input class="form-control" />
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Описание</label>
+        <div class="input-group">
+          <textarea rows="3" class="form-control"></textarea>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="form-label">Изображение</label>
+        <div class="image-uploader">
+          <label class="image-uploader__preview">
+            <span>Загрузить изображение</span>
+            <input type="file" accept="image/*" class="form-control-file" />
+          </label>
+        </div>
+      </div>
+    </fieldset>
   </form-layout>
 </template>
 
